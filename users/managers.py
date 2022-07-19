eaf7eaafc36314fb92c975af60c04799d81e9ccb
PauloJose26 <paulojoliveira.2616@gmail.com>
2022-07-19 11:14:49 -0300

    feat: created user models

---
+++ users/managers.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    def _create_user(self, email: str, password: str, **kwargs: dict):
+        if not email or not password:
+            raise ValueError({ "error": "The email and password must be set" })
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, **kwargs)
+        
+        user.set_password(password)
+        user.save(using=self.db)
+        
+        return user
+    
+    
+    def create(self, email: str, password: str, **kwargs: dict):
+        kwargs.setdefault("is_staff", False)
+        kwargs.setdefault("is_superuser", False)
+        
+        return self._create_user(email, password, **kwargs)
+    
+    
+    def create_superuser(self, email: str, password: str, **kwargs: dict):
+        kwargs.setdefault("is_staff", True)
+        kwargs.setdefault("is_superuser", True)
+        
+        return self._create_user(email, password, **kwargs)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,25 @@
+from uuid import uuid4
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+from users.managers import UserManager
+
+
+class User(AbstractUser):
+    username = None
+    
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    password = models.CharField(max_length=200)
+    avatar = models.TextField()
+    
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    
+    
+    objects = AbstractUser
+    
+    USERNAME_FIELD: str = "email"
+    REQUIRED_FIELDS = [ "first_name", "last_name", "password" ]
