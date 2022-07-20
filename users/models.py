f59aa8b97f5a64b86508bde437344d0422b6e8a3
bercardosoc <bernardocardosodacosta@gmail.com>
2022-07-20 10:10:15 -0300

    feat: create users_list model

--- users/models.py
+++ users/models.py
@@ -18,8 +18,23 @@ class User(AbstractUser):
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
     
+    animes = models.ManyToManyField("animes.Anime", through="Userlist")
     
     objects = UserManager()
     
     USERNAME_FIELD: str = "email"
     REQUIRED_FIELDS = [ "first_name", "last_name", "password" ]
+
+ 
+class ChoicesStatus(models.TextChoices):
+
+    WATCH_LATER = ("Assistir mais tarde")
+    WATCHING = ("Assistindo")
+    FINISHED = ("Terminado")
+
+class Userlist(models.Model):
+
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    anime = models.ForeignKey("animes.Anime", related_name="anime", on_delete=models.CASCADE)
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    watching_status = models.CharField(max_length=50, choices=ChoicesStatus.choices)
