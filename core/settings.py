4ba837dd91228aff1e13561a6b1c62a364995b54
Bruno Tetzner <brunoltetzner@gmail.com>
2022-07-21 13:41:30 -0300

    heroku config
    Signed-off-by: Bruno Tetzner <brunoltetzner@gmail.com>

--- core/settings.py
+++ core/settings.py
@@ -21,6 +21,7 @@ if DATABASE_URL:
     db_from_env = dj_database_url.config(
         default=DATABASE_URL, conn_max_age=500, ssl_require=True)
     DATABASES['default'].update(db_from_env)
+    debug=True
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
