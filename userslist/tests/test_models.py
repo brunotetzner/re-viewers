381250f8eb07a2d02e02cd480c472b80886e36fc
bercardosoc <bernardocardosodacosta@gmail.com>
2022-07-22 13:30:54 -0300

    test: init tests

---
+++ userslist/tests/test_models.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+from ...users.models import Userlist
+from ...users.models import User 
+from ...animes.models import Anime
+
+class UserlistModelTest(TestCase):
+    pass
+    #@classmethod
+    
