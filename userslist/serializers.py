cfad8a65a51b639679d2078e55bee36401785d73
bercardosoc <bernardocardosodacosta@gmail.com>
2022-07-22 09:13:16 -0300

    refactor: remove unused comment

--- userslist/serializers.py
+++ userslist/serializers.py
@@ -10,19 +10,3 @@ class UserListSerializer(serializers.ModelSerializer):
         model = Userlist
         fields = "__all__"
         read_only_fields = ["user", "anime"]
-
-        
-
-    """ def create(self, validated_data):
-
-        user_id = validated_data.get("user_id")
-        anime_id = validated_data.get("anime_id")
-
-        user = get_object_or_404(User, pk=user_id)
-        anime = get_object_or_404(Anime, pk=anime_id)
-        
-        validated_data["user"] = user 
-        validated_data["anime"] = anime 
-        user_list = Userlist.objects.create(**validated_data)
-
-        return user_list """
