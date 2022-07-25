edf13f35aa277a71f1f62bdf16386a0e96e4e665
bercardosoc <bernardocardosodacosta@gmail.com>
2022-07-25 20:35:58 -0300

    refactor: remove cls method from test view

--- userslist/tests/test_view.py
+++ userslist/tests/test_view.py
@@ -1,100 +1,78 @@
 from rest_framework.test import APITestCase
 from rest_framework.authtoken.models import Token
-
-from users.models import Userlist
+from users.models import User
 
 class UserslistViewsText(APITestCase):
-    @classmethod
-    def setUpTestData(cls) -> None:
-
-        cls.anime = {
-            "id": "sauhsaushaushua",
-            "image":"jojo.png",
-            "title":"jojo",
-            "sinopse":"jojo",
-            "studio":"jojo",
-            "banner":"jojojo.png",
-            "original_title":"jojo",
-            "launch_data":"jojo",
-            "average_rate":"jojo",
-            "status":"On Going",
-        }
-
-        cls.user = {
-            "email":"bernardo@mail.com",
-            "first_name":"bernardo",
-            "last_name":"costa",
-            "password":"loona",
-        }
-
-        cls.anime_to_list = {
-            "watching_status": "Terminado"
-        }
-
-    def test_user_adding_anime_to_userlist(self):
-
-        self.client.post(
-            "api/users/register/", 
-            self.user, 
-            format="json"
-            )
-
-        login_data = {
-            "email": self.user["email"],
-            "password": self.user["password"],
-        }
-        
-        auth = self.client.post(
-            "/api/users/login/", 
-            login_data, 
-            format="json"
-            )
-
-        token = auth.data['token']
-        # token = auth["token"]
-
-        self.client.credentials(
-            HTTP_AUTHORIZATION=f"Token " + token
+    def setUp(self):
+        user = User.objects.create(
+            first_name="user",
+            last_name="last_name",
+            password="password",
+            email="test@mail.com",
+            is_superuser=True,
         )
+        token = Token.objects.create(user=user)
 
-        anime = self.anime
-        anime_id = anime["id"]
-
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + token.key)
+        
+    def test_post_view(self):
+        anime = self.client.post(
+            "/api/animes/",
+            {
+                "image": "logeo.png",
+                "title": "The witceher",
+                "sinopse": "blableabla",
+                "studio": "marqeuinho produções",
+                "banner": "bannere.png",
+                "original_title": "the witcher",
+                "launch_data": "2022-07-15",
+                "status": "Finished",
+                "categories": [{"category": "açeão"}, {"category": "batata"}],
+                "average_rate": 0,
+            },
+            format="json",
+        )
+        
         response = self.client.post(
-            f"/api/userlist/{anime_id}/",
-            # f"/api/userlist/sauhsaushaushua/",
-            self.anime_to_list,
-            format="json"
-            )
-        
-        self.assertEquals(response.status_code, 201)
+            f'/api/userlist/{anime.data["id"]}/',
+            {"watching_status": "Terminado"},
+            format="json",
+        )
+        self.assertEqual(response.status_code,201)
 
-    def test_userlist_getting(self):
+    def test_list_all_view(self):
+        response = self.client.get("/api/userlist/")
+        self.assertEqual(response.status_code, 200)
 
-        self.client.post(
-            "api/users/register/", 
-            self.user, 
-            format="json"
-            )
 
-        login_data = {
-            "email": self.user["email"],
-            "password": self.user["password"],
-        }
-        
-        auth = self.client.post(
-            "/api/users/login/", 
-            login_data, 
-            format="json"
-            )
-
-        token = auth.data['token']
-        # token = auth["token"]
-
-        self.client.credentials(
-            HTTP_AUTHORIZATION="Token " + token
+    def test_update_view(self):
+        anime = self.client.post(
+            "/api/animes/",
+            {
+                "image": "logeo.png",
+                "title": "The witceher",
+                "sinopse": "blableabla",
+                "studio": "marqeuinho produções",
+                "banner": "bannere.png",
+                "original_title": "the witcher",
+                "launch_data": "2022-07-15",
+                "status": "Finished",
+                "categories": [{"category": "açeão"}, {"category": "batata"}],
+                "average_rate": 0,
+            },
+            format="json",
         )
 
-        response = self.client.get("/api/userlist/")
-        
+        my_anime = self.client.post(
+            f'/api/userlist/{anime.data["id"]}/',
+            {"watching_status": "Terminado"},
+            format="json",
+        )
+
+        response = self.client.patch(
+            f'/api/userlist/myanimes/{my_anime.data["id"]}/',
+            {"watching_status": "Terminado"},
+            format="json",
+        )
+
         self.assertEqual(response.status_code, 200)
