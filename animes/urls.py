97209ca131e3480ae2cf6b8400b2c1e7526756a3
Gustavo <gu.machado.oliveira@gmail>
2022-07-26 17:50:00 -0300

    feat: create generic view to merge patch and delete route

--- animes/urls.py
+++ animes/urls.py
@@ -3,5 +3,5 @@ from .views import AnimeView, AnimeIdView
 
 urlpatterns = [
     path("animes/", AnimeView.as_view()),
-    path("animes/<str:anime_id>/", AnimeIdView.as_view()),
+    path("animes/<id>/", AnimeIdView.as_view()),
 ]
